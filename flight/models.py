ee1d4b4cf8eb63f4a771fd0f83b45b4215ae0873
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-24 12:26:55 +0300

    done

--- flight/models.py
+++ flight/models.py
@@ -28,6 +28,6 @@ class Passenger(models.Model):
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    passenger = models.ManyToManyField(Passenger, related_name='passengers')
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='reservation')
+    passenger = models.ManyToManyField(Passenger, related_name='reservations')
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='reservations')
     
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Flight, Passenger, Reservation
 
-admin.site.register(Flight)
-admin.site.register(Passenger)
-admin.site.register(Reservation)
+# Register your models here.
